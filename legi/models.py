a676709992670f6ca6271fb65c1b8793733db660
drewablo <andrewt77@gmail.com>
2014-06-04 09:25:26 -0500

    Update models.py

--- legi/models.py
+++ legi/models.py
@@ -6,11 +6,16 @@ class Chamber(models.Model):
 	legislation = models.CharField(max_length=8)
 	actions = models.CharField(max_length=100)
 	dt =  models.CharField(max_length=10)
+	party = models.CharField(max_length=1)
 	
 	def billNumber(self):
 		return self.legislation
 	def name(self):
-		return self.legislator:
+		return self.legislator
+	def date(self):
+		return self.dt
+	def pol(self):
+		return self.party
 
 class Votes(models.Model):
 	legislator = models.CharField(max_length=60)

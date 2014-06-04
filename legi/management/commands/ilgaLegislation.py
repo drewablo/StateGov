56f8ac9944fbc63d76d40f19682f49c9c0005e64
drewablo <andrewt77@gmail.com>
2014-06-04 07:35:22 -0500

    added party

--- legi/management/commands/ilgaLegislation.py
+++ legi/management/commands/ilgaLegislation.py
@@ -10,6 +10,7 @@ class Command(BaseCommand):
 		chmbrs =['http://www.ilga.gov/house/', 'http://www.ilga.gov/senate/']
 		chamber_abbr = ['HB','SB']
 		c = 0
+		partyAffil =[]
 		for chmbr in chmbrs:
 			site = chmbr	
 			url = urllib2.urlopen(site)
@@ -17,14 +18,27 @@ class Command(BaseCommand):
 			soup = BeautifulSoup(content)
 			links = []
 			x=0
+			y=-1
 			table = soup.find('table', cellpadding=3)
 			for a in soup.findAll('a',href=True):
 				if re.findall('Bills', a['href']):
 					l = (site + a['href']+'&Primary=True')
 					links.append(str(l))
 					x+=1
+					print len(links)
+					print"links"
+			for p in table.findAll('tr')[2:]:
+				col = p.findAll('td')
+				party = col[4].string
+				partyAffil.append(party)
 			chamber_abbr = chamber_abbr[c]
+			partyAffil.append('D')
+			partyAffil.append('D')
+			partyAffil.append('R')
+			partyAffil.append('R')
+			print len(partyAffil)
 			for link in links:
+				y+=1
 				url = urllib2.urlopen(link)
 				content = url.read()
 				soup = BeautifulSoup(content)
@@ -39,16 +53,19 @@ class Command(BaseCommand):
 					for z in re.findall('2014', last_action_date):
 						for q in re.findall(chamber_abbr, bill):
 							if Chamber.objects.filter(legislation=bill).exists() == False:
-								s = Chamber(legislator=sponsor, legislation=bill, actions=last_action, dt=last_action_date)
+								s = Chamber(legislator=sponsor, legislation=bill, actions=last_action, dt=last_action_date, party=partyAffil[y])
 								s.save()
-								print 'New Record Found'
+
 							elif Chamber.objects.filter(legislation=bill).exists() == True:
 								s = Chamber.objects.get(legislation=bill)
+								s.party=partyAffil[y]
+								s.save()
 								if s.actions.encode('ascii')!=last_action:
 									s.actions=last_action
 									s.save()
 									s.dt=last_action_date
 									s.save()
-									print 'Record Updated'
+
+
 
 			c +=1

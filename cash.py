b0ed0658b51cbacbe8ffa7f6e8b0b2a46dea8130
drewablo <andrewt77@gmail.com>
2014-06-11 09:33:00 -0500

    Update cash.py

--- cash.py
+++ cash.py
@@ -1,53 +1,15 @@
-
-from bs4 import BeautifulSoup
-import urllib2
-import re
-import os
+from xlml import etree
 import csv
+import re
 
-money = str()
-zips = str()
 moneyZips = open('moneyZips.csv', 'wb')
 writer = csv.writer(moneyZips)
-site = "http://bit.ly/2014ilmoney"
-url = urllib2.urlopen(site).read()
-soup = BeautifulSoup(url)
-table = soup.find('table', {'id': 'tblContributionListPrint'})
-for item in table.findAll('tr')[2:]:
-    for row in item.findAll('td'):
-        for span in row.findAll('span'):
-            if re.findall(r'(^\$)', span.text):
-                try:
-                        if re.findall(r'(\$?[\d,]*\d,\d{3}\.\d{2})', span.text):
-                                money = re.findall(r'(\$?[\d,]*\d,\d{3}\.\d{2})', span.text)
-                        elif re.findall(r'(\$?[\d{2},]*\d{2},\d{3}\.\d{2})', span.text):
-                                money = re.findall(r'(\$?[\d{2},]*\d{2},\d{3}\.\d{2})', span.text)
-                        elif re.findall(r'(\$?[\d{3},]*\d,\d{3}\.\d{2})', span.text):
-                                money = re.findall(r'(\$?[\d{3},]*\d,\d{3}\.\d{2})', span.text)
-                        elif re.findall(r'(\$\d\.\d{2})', span.text):
-                                money = re.findall(r'(\$\d)\.\d{2}', span.text)
-                        elif re.findall(r'(\$\d+\.\d{2})', span.text):
-                                money = re.findall(r'(^\$\d+\.\d{2})', span.text)
-                except:
-                        pass
-            elif re.findall(r'(\d{5})', span.text):
-                zips =  re.findall(r'(\d{5})', span.text)
 
-        if len(zips) > 1:
-                zips.pop(0)
-                z = str(zips[0])
-                m = str(money[0])
-                master = (z,m)
-                writer.writerow(master)
-        elif len(zips) == 1:
-                try:
-                        z = str(zips[0])
-                        m = str(money[0])
-                        master =(z,m)
-                        writer.writerow(master)
-                except:
-                        pass
-        else:
-                print 'error'
-
-moneyZips.close()
+xml = (/Recipts.xml)
+context = etree.interparse(xml, tag='RECEIPTLIST')
+for c in context: 
+	zips = str(c.xpath('RECEIPT/Zip/text()'))
+	money = str(c.xpath('RECEIPT/Amount.text()'))
+	if re.findall(r'(-)', zips):
+		zips = re.findall(r'(^\d{5})', zips)
+	writer.writerow(zips,money)

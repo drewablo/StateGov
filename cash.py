c2cf9fa2a8682ee380fc51bb73ed115f54383f91
drewablo <andrewt77@gmail.com>
2014-06-11 09:41:37 -0500

    Update cash.py

--- cash.py
+++ cash.py
@@ -1,15 +1,22 @@
-from xlml import etree
+from lxml import etree
 import csv
 import re
 
 moneyZips = open('moneyZips.csv', 'wb')
 writer = csv.writer(moneyZips)
 
-xml = (/Recipts.xml)
-context = etree.interparse(xml, tag='RECEIPTLIST')
-for c in context: 
-	zips = str(c.xpath('RECEIPT/Zip/text()'))
-	money = str(c.xpath('RECEIPT/Amount.text()'))
-	if re.findall(r'(-)', zips):
-		zips = re.findall(r'(^\d{5})', zips)
-	writer.writerow(zips,money)
+xml = ('Receipts.xml')
+context = etree.iterparse(xml, tag='RECEIPTLIST')
+for action, elem in context:
+        zips = elem.xpath('RECEIPT/Zip/text()')
+        money = elem.xpath('RECEIPT/Amount/text()')
+        if re.findall(r'(-)', zips):
+                zips = re.findall(r'(^\d{5})', zips)
+        writer.writerow(zips,money)
+
+
+
+
+
+
+
